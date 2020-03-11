7d802d9ca367cd0f997e16aea31e3b01d406dea3
Elif <koseler17@itu.edu.tr>
2020-03-11 10:28:04 +0300

    Add files via upload

---
+++ Creating A Sound/sound2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.io.wavfile as wavfile
+
+n = 6
+x = np.arange(n)
+
+y = np.sin(np.pi*x)+np.cos(np.pi*x)
+
+y = np.tile(y,1000)
+
+wavfile.write("sound1.wav",5000,y)
